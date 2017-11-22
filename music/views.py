15a596ecea831565bfdce8c04c2b1cba0394a63a
taha <taha.maqbool96@gmail.com>
2017-11-22 17:26:08 +0500

    added models and templates

--- music/views.py
+++ music/views.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render
+from models import Album
 
 def index(request):
-    return HttpResponse('<h1>Music Section<h1>')
+    albums = Album.objects.all()
+    return render(request,'music/index.html',{'albums': albums})
+
+def details(request,album_id):
+    try:
+        album = Album.objects.get(id = album_id)
+    except Album.DoesNotExist:
+        raise Http404("This Album does not exists!")
+    return render(request,'music/detail.html',{'album':album})
